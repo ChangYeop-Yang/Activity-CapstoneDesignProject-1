41c929e4e5204dd18785bb840ad1eb5f6a868972
양창엽 <yeop9657@naver.com>
2017-09-14 22:48:25 +0900

    [Capstone Design 1] Team School Health1st Android Project. (Prof. 고석주) 20170914 Init Update

---
+++ app/src/main/java/com/health1st/yeop9657/health1st/MainActivity.java
@@ -0,0 +1,13 @@
+package com.health1st.yeop9657.health1st;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
