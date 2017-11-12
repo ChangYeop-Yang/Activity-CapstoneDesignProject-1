a6ef9cbe5e20ba9907e96ec088adc73e79af91c3
양창엽 <ChangYeop-Yang@users.noreply.github.com>
2017-11-12 23:10:35 +0900

    [Capstone Design 1] Team School Health1st Android Project. (Prof. 고석주) FHIR 20171112 23:10:46 Update.

--- app/src/main/java/com/health1st/yeop9657/health1st/Accessory/BluetoothManager.java
+++ app/src/main/java/com/health1st/yeop9657/health1st/Accessory/BluetoothManager.java
@@ -34,6 +34,25 @@ public class BluetoothManager extends Thread {
         public static final UUID HEART_CONTROL_CHARACTERISTIC = UUID.fromString("00002a39-0000-1000-8000-00805f9b34fb");
     }
 
+    public final static class PAARBand {
+        public static final String PAAR_WATCH_CLASSIC = "00001101-0000-1000-8000-00805f9b34fb";
+        public static String PAAR_WATCH_NOTIFY_CHARACTERISTIC = "6e407f01-b5a3-f393-e0a9-e50e24dcca9e";
+        public static String PAAR_WATCH_SERVICE = "6e402650-b5a3-f393-e0a9-e50e24dcca9e";
+        public static String TI_PAAR_WATCH_NOTIFY_CHARACTERISTIC = toUuid128StringFormat(32513);
+        public static String TI_PAAR_WATCH_SERVICE = toUuid128StringFormat(9808);
+        public static String TI_PAAR_WATCH_WRITE_CHARACTERISTIC = toUuid128StringFormat(32514);
+        public static String pAAR_WATCH_WRITE_CHARACTERISTIC = "6e407f02-b5a3-f393-e0a9-e50e24dcca9e";
+
+        private final static String toUuid128StringFormat(int parm) {
+            final StringBuilder mStringBuilder = new StringBuilder("0000");
+            mStringBuilder.append(Integer.toHexString(parm));
+            mStringBuilder.append("-0000-1000-8000-00805f9b34fb");
+            return mStringBuilder.toString();
+        }
+    }
+
+    protected static final long SCAN_PERIOD = 10000;
+
     /* TODO - : Vibrate Method */
     protected void startDeviceVibrate(final Context mContext) {
         if (mVibrator == null) { mVibrator = (Vibrator)mContext.getSystemService(Context.VIBRATOR_SERVICE); }
